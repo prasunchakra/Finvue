f60c23e156c822c0a2740cd58a6eb8a4badc88de
prasunchakra <lordprasun@gmail.com>
2023-08-27 18:53:43 +0530

    Just starting up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  items: MenuItem[] | undefined;
   title = 'finvue';
+  ngOnInit() {
+    this.items = [
+      { label: 'Home', icon: 'pi pi-fw pi-home', url: '/' },
+      { label: 'Features', icon: 'pi pi-fw pi-list', url: '#features' },
+      { label: 'Contact', icon: 'pi pi-fw pi-envelope', url: '/contact' },
+    ];
+  }
+  features = [
+    {
+      icon: 'pi pi-chart-bar',
+      title: 'Dynamic Visualization',
+      description: 'Visualize your finances in dynamic charts.',
+    },
+    {
+      icon: 'pi pi-dollar',
+      title: 'Smart Investments',
+      description: 'Get insights for smart investment decisions.',
+    },
+    // ... more features
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MenubarModule } from 'primeng/menubar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MenubarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
